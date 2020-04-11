3770b68ba715683bd638dfc3a9ea929cfbc312e9
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-04-11 21:49:47 +0200

    Test change

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ const { mapPandaScoreResult } = require("./lib/pandascore-utils");
 const lcsId = 4198;
 
 async function main() {
+    // Test
     const result = await pandascore.getUpcomingMatches(lcsId);
     const mapped = mapPandaScoreResult(result);
     console.log(JSON.stringify(mapped));
