18d675f90a326ef2c887b58b2284d3a08266e53e
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-05-09 00:19:41 +0200

    Add links to icals in README.md

--- _scripts/get-leagues.js
+++ _scripts/get-leagues.js
@@ -6,14 +6,25 @@ main();
 async function main() {
     const leagues = (await getLeagues()).map((l) => ({
         name: l.name,
+        slug: l.slug,
         image_url: l.image_url,
         url: l.url,
     }));
 
-    console.log(renderLeages(leagues));
+    console.log(renderLeagueTable(leagues));
+    // console.log(renderLeagues(leagues));
 }
 
-function renderLeages(leagues) {
+function renderLeagueTable(leagues) {
+    return leagues
+        .map(
+            (league) =>
+                `| <img src="${league.image_url}" alt="${league.name} Logo" width="24" height="24" /> | ${league.name} | [Get ical here](output/${league.slug}.ical)|`,
+        )
+        .join("\n");
+}
+
+function renderLeagues(leagues) {
     return leagues.map(renderSingleLeague).join("\n");
 }
 
