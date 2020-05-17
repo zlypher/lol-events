5b69703415654cfb75e5b166b4924e2c102e9ef2
Thomas Prochazka <thomas.prochazka@gmail.com>
2020-05-17 13:35:02 +0200

    Start to prototype website for lol-events

---
+++ web/index.js
@@ -0,0 +1,8 @@
+console.log("hello World");
+
+document.addEventListener("change", (evt) => {
+    console.log("change", evt);
+
+    // TODO: On change, update textfield
+    // TODO: Add btn to quickly copy content of textfield
+});
